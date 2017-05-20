a6b3d37e6cafebf114437668688a4399333eeee0
周文侃 <wkzhou.1988@gmail.com>
2017-05-20 09:44:51 +0800

    no message

--- CircleGame/Assets/Scripts/Plate.cs
+++ CircleGame/Assets/Scripts/Plate.cs
@@ -6,7 +6,7 @@ using System.Text;
 using UnityEngine.UI;
 
 public static class globalConfig{
-	public static float radius = 3.0f;
+	public static float radius = 5.0f;
 	public static Dictionary<string,string> colorMusicPair = new Dictionary<string, string> (){
 		{"245,138,172","do"},
 		{"45,197,201","re"},
@@ -81,7 +81,7 @@ public class Plate : MonoBehaviour {
 
 	void Start(){
 		if (Camera.main.GetComponent<EditorControl> () == null) {
-			open("2-1");
+			open("1-1");
 		}
 	}
 	
@@ -122,7 +122,7 @@ public class Plate : MonoBehaviour {
 			sector_rotations [i] = config [i].rotation;
 		}
 
-		drawFrontier ();
+//		drawFrontier ();
 		drawSector ();
 	}
 
